ab1c43d9fc03865c93ef6b239213fa508157caab
CharlieCrisp <charliecrisp@improbable.io>
2018-11-20 20:29:22 +0000

    Finish changes to route tiles

--- js/set_on_clicks.js
+++ js/set_on_clicks.js
@@ -5,12 +5,12 @@ for (var i = 0; i < routeTiles.length; i++) {
 
   const onMouseEnter = function() {
     const data = route.dataset;
-    route.setAttribute("style", `background: linear-gradient(-45deg, rgba(${data.x}, ${data.y} , ${data.z}, 0.75), rgba(${data.x}, ${data.y} , ${data.z}, 0.75)) 0% 0% / contain, url(/assets/${data.iconUrl}) center center no-repeat;`);
+    route.setAttribute("style", `background: linear-gradient(-45deg, rgba(${data.x}, ${data.y} , ${data.z}, 0.75), rgba(${data.x}, ${data.y} , ${data.z}, 0.75)) 0% 0%, url(/assets/${data.iconUrl}) center center no-repeat; background-size: contain;`);
   }
 
   const onMouseLeave = function() {
     const data = route.dataset;
-    route.setAttribute("style", `background: linear-gradient(-45deg, rgba(${data.x}, ${data.y} , ${data.z}), rgba(${data.x}, ${data.y} , ${data.z}, 0.55)) 0% 0% / contain, url(/assets/${data.iconUrl}) center center no-repeat;`);
+    route.setAttribute("style", `background: linear-gradient(-45deg, rgba(${data.x}, ${data.y} , ${data.z}), rgba(${data.x}, ${data.y} , ${data.z}, 0.55)) 0% 0%, url(/assets/${data.iconUrl}) center center no-repeat; background-size: contain;`);
   }
 
   route.onmouseenter = onMouseEnter;
